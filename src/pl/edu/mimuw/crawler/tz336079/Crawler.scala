328ecb30cf59289db936678459914dbb5908bc9f
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-06 22:00:56 +0200

    First steps towards class design (#1).

---
+++ src/pl/edu/mimuw/crawler/tz336079/Crawler.scala
@@ -0,0 +1,7 @@
+package pl.edu.mimuw.crawler.tz336079
+
+class Crawler {
+	//TODO: implement
+	//this shall be the main class of the whole project,
+	//the user should use
+}
---
+++ src/pl/edu/mimuw/crawler/tz336079/Node.scala
@@ -0,0 +1,5 @@
+package pl.edu.mimuw.crawler.tz336079
+
+class Node {
+
+}
