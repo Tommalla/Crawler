d3ce36fb113c3366545abe386e21e756b247b0fd
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-08 14:14:25 +0200

    Further SitesQueue implementation. #5

--- src/pl/edu/mimuw/crawler/tz336079/SitesQueue.scala
+++ src/pl/edu/mimuw/crawler/tz336079/SitesQueue.scala
@@ -1,8 +1,21 @@
 package pl.edu.mimuw.crawler.tz336079
 import scala.collection.mutable.Queue
 
-object SitesQueue extends Queue[Node] {
+//TODO fiddle with private, package etc.
+
+object SitesQueue {
+	private var queue: Queue[Node] = new Queue[Node]();
+
 	def addURL(url: String): Unit = {
-		//TODO
+		//TODO find URL in Graph
+		//if present - append Node,
+		//else, - create node in graph and append it
+	}
+
+	def getFront(): Option[Node] = {
+		if (queue.size > 0)
+			queue.dequeue().asInstanceOf[Option[Node]];
+		else
+			None;
 	}
 }
