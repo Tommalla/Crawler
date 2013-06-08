ed8558883a26e3d4ca30a7e79cc166b4d947ba4c
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-08 14:30:27 +0200

    Further Node implementation. #7

--- src/pl/edu/mimuw/crawler/tz336079/Node.scala
+++ src/pl/edu/mimuw/crawler/tz336079/Node.scala
@@ -1,5 +1,12 @@
 package pl.edu.mimuw.crawler.tz336079
 
-class Node {
+class Node(url: String) {
+	var neighbours: List[Node] = Nil;
 
+	//processes the node (TODO: add action!)
+	def process(/* Site action here */): Unit = {
+		//TODO implement:
+		//create the list of neighbours from urls,
+		//use SiteAction to collect statistics
+	}
 }
