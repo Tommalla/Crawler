e82ce085424369812190a0596ab8d4127a666526
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-08 21:52:23 +0200

    A somewhat finished implementation of Crawler. Closes #4

--- src/pl/edu/mimuw/crawler/tz336079/Crawler.scala
+++ src/pl/edu/mimuw/crawler/tz336079/Crawler.scala
@@ -1,16 +1,20 @@
 package pl.edu.mimuw.crawler.tz336079
 
-class Crawler {
+class Crawler(action: SiteAction) {
 
 	private var isRunning: Boolean = false;
 
 	def start(): Unit = {
 		isRunning = true;
 		while (isRunning && !SitesQueue.empty())
-			SitesQueue.getFront().get.process(/*Add SiteAction*/);
+			SitesQueue.getFront().get.process(action);
 	}
 
 	def stop(): Unit = {
 		isRunning = false;
+	}
+
+	def addTargetURL(url: String): Unit = {
+		SitesQueue.addURL(url);
 	}
 }
