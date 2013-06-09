4476a8a5625f18e6b809953e285aceb6a9795a67
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-09 12:37:29 +0200

    Final bugfixes. Closes #11.

--- src/demos/Demo1.scala
+++ src/demos/Demo1.scala
@@ -1,14 +1,20 @@
 package demos
 
 import pl.edu.mimuw.crawler.tz336079._;
+import java.lang.IllegalArgumentException;
 
 object Demo1 {
 	def main(args: Array[String]) = {
+		if (args.length < 1)
+			throw new IllegalArgumentException("Not enough arguments!");
+		if (Methods.isURLValid(args(0)) == false)
+			throw new IllegalArgumentException("Malformed url!");
+
 		val params: NullParameters = new NullParameters();
-		val action: DomainCounterAction = new DomainCounterAction("www.mimuw.edu.pl");
+		val action: DomainCounterAction = new DomainCounterAction(Methods.getBaseDomain(args(0)));
 		val crawler: Crawler = new Crawler(action, params);
 
-		crawler.addTargetURL("http://www.mimuw.edu.pl/~kdr/test/1/");
+		crawler.addTargetURL(args(0));
 		crawler.start();
 		for ((site, qty) <- action.getResults()) {
 			println(site + ": " + qty);
---
+++ src/pl/edu/mimuw/crawler/tz336079/Methods.scala
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.edu.mimuw.crawler.tz336079
+
+import scala.util.matching.Regex;
+
+object Methods {
+	val External = new Regex("""http://([^/]*)(/.*){0,1}""", "domain", "locPath");
+	val Local = new Regex("""(.*)/([^/]*)""");
+
+	def getBaseDomain(url: String): String = url match {
+		case External(domain, _) => domain;
+		case _ => throw new Exception("Methods.getBaseDomain: " + url + " is a malformed url");
+	}
+
+	def isURLValid(url: String): Boolean = url match {
+		case External(_, _) => true;
+		case _ => false;
+	}
+}
