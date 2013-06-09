34a4230bf27607dcf350aa5b152d80185e1b8daa
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-09 10:04:21 +0200

    Implemented Demo1. Closes #2.

--- src/demos/Demo1.scala
+++ src/demos/Demo1.scala
@@ -1,5 +1,15 @@
 package demos
 
-object Demo1 {
+import pl.edu.mimuw.crawler.tz336079._;
 
+object Demo1 {
+	def main(args: Array[String]) = {
+		val params: NullParameters = new NullParameters();
+		val action: DomainCounterAction = new DomainCounterAction();
+		val crawler: Crawler = new Crawler(action, params);
+
+		crawler.addTargetURL("http://www.mimuw.edu.pl/~kdr/test/1/");
+		crawler.start();
+		println(action.getResults());
+	}
 }
