6100319d1783377d24cbc7bab4da2d51d1513936
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-10 11:21:25 +0200

    Added documentation to Parameters and DepthParameters. Closes #21,#16

--- src/pl/edu/mimuw/crawler/tz336079/DepthParameters.scala
+++ src/pl/edu/mimuw/crawler/tz336079/DepthParameters.scala
@@ -1,6 +1,14 @@
 package pl.edu.mimuw.crawler.tz336079;
 
+/**
+ * [[Parameters]] implementation that introduces depth to the search.
+ *
+ * @param depth The current depth.
+ */
 case class DepthParameters(depth: Int) extends Parameters {
+	/**
+	 * Returns new [[DepthParameters]] with depth + 1 .
+	 */
 	def getChild(): Parameters = {
 		new DepthParameters(depth + 1);
 	};
--- src/pl/edu/mimuw/crawler/tz336079/Parameters.scala
+++ src/pl/edu/mimuw/crawler/tz336079/Parameters.scala
@@ -1,5 +1,11 @@
 package pl.edu.mimuw.crawler.tz336079
 
+/**
+ * A trait the user has to implement in order to have own parameters for the
+ * Nodes that could limit the graph search and building in any way.
+ *
+ * See [[SiteAction]].
+ */
 trait Parameters {
 	/**
 	 * Returns parameters for the child node
