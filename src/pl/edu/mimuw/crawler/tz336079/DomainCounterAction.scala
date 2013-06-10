f7565c9744eb997cb15b28ba13091bccba713bdf
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-10 11:28:10 +0200

    Added scaladoc to DomainCounterAction. Closes #20.

--- src/pl/edu/mimuw/crawler/tz336079/DomainCounterAction.scala
+++ src/pl/edu/mimuw/crawler/tz336079/DomainCounterAction.scala
@@ -4,15 +4,29 @@ import _root_.org.jsoup.nodes._;
 import _root_.org.jsoup.select._;
 import scala.collection.immutable.HashMap;
 
-
+/**
+ * An implementation of [[SiteAction]] that counts external domains accessible
+ * from the site.
+ *
+ * @param baseDomain The base domain of the site.
+ */
 class DomainCounterAction(baseDomain: String) extends SiteAction {
 	private var errors: List[String] = Nil;
 	private var resMap: HashMap[String, Int] = new HashMap[String, Int]();
 
+	/**
+	 * Adds errorInfo to the internal list of errors
+	 */
 	def handleError(errorInfo: String): Unit = {
 		this.errors = errorInfo::this.errors;
 	}
 
+	/**
+	 * Adds all external urls to the local map.
+	 *
+	 * @return false if the domain of the currently processed site is
+	 * different from [[baseDomain]]. Otherwise true.
+	 */
 	def process(doc: Document, params: Parameters): Boolean = {
 		if ( Methods.isURLExternal(doc.baseUri) && Methods.getBaseDomain(doc.baseUri) != baseDomain )
 			return false;
@@ -30,11 +44,17 @@ class DomainCounterAction(baseDomain: String) extends SiteAction {
 		true;
 	}
 
+	/**
+	 * Returns a sorted list of pairs (domain, number of occurences)
+	 */
 	def getResults(): List[(String, Int)] = {
 		var tmp: List[(String, Int)] = resMap.foldLeft[List[(String, Int)]](Nil)((acc, x) => x::acc)
 		tmp.sortWith({ case ((keyA, valA), (keyB, valB)) => if (valA != valB) valA > valB else keyA < keyB})
 	}
 
+	/**
+	 * Returns a list of errors.
+	 */
 	def getErrors(): List[String] = {
 		this.errors;
 	}
