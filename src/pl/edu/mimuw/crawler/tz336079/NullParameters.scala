8b29c076a5972833cee7d2d99eff9562aeaf89ea
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-10 11:14:30 +0200

    Added documentation to NullParameters. Closes #18.

--- src/pl/edu/mimuw/crawler/tz336079/NullParameters.scala
+++ src/pl/edu/mimuw/crawler/tz336079/NullParameters.scala
@@ -4,10 +4,16 @@ package pl.edu.mimuw.crawler.tz336079
  * A simple class providing 'Null' Parameters that do nothing.
  */
 case class NullParameters extends Parameters {
+	/**
+	 * Returns self.
+	 */
 	def getChild(): Parameters = {
 		this;
 	}
 
+	/**
+	 * Returns true.
+	 */
 	def canContinue(): Boolean = {
 		true;
 	}
