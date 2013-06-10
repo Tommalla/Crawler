daca39c821bee54a2ba8d5ea57b1051591ca8fe7
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2013-06-10 10:49:55 +0200

    Added SiteAction documentation. Closes #17.

--- src/pl/edu/mimuw/crawler/tz336079/SiteAction.scala
+++ src/pl/edu/mimuw/crawler/tz336079/SiteAction.scala
@@ -3,13 +3,38 @@ package pl.edu.mimuw.crawler.tz336079
 import _root_.org.jsoup.nodes._;
 import _root_.org.jsoup.select._;
 
+/**
+ * The trait that user has to implement and pass to the [[Crawler]] in order to
+ * perform any action on the sites.
+ */
 trait SiteAction {
-	implicit def iteratorToWrapper[T](iter:java.util.Iterator[T]):IteratorWrapper[T] = new IteratorWrapper[T](iter);
+	/**
+	 * A necessary java collections iterator wrapper for scala.
+	 */
+	protected implicit def iteratorToWrapper[T](iter:java.util.Iterator[T]):IteratorWrapper[T] = new IteratorWrapper[T](iter);
 
+	/**
+	 * This method is called by the [[Node]] on every site it decides to
+	 * visit.
+	 *
+	 * @param doc The full [[Document]] that is being parsed.
+	 * @param params Current node [[Parameters]]
+	 *
+	 * @return It's up to the implementation of [[SiteAction]] to decide
+	 * whether it wants to go deeper into the links of this document or not.
+	 * Returning false will stop the process.
+	 */
 	def process(doc: Document, params: Parameters): Boolean;
+	/**
+	 * Whenever an error occurs, the [[Node]] will call this method with the
+	 * exception message.
+	 */
 	def handleError(errorInfo: String): Unit;
 
-	def getAllURLs(doc: Document): List[String] = {
+	/**
+	 * Returns all the urls introduced by the <a> tag from the [[Document]]
+	 */
+	protected def getAllURLs(doc: Document): List[String] = {
 		val anchors: Elements = doc.select("a");
 		var res: List[String] = Nil;
 		for (anchor <- anchors.iterator()) {
